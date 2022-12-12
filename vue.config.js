a12057bc4c4fb28f3e9f428c69774d056c939e00
hwhany123 <253922804@qq.com>
2022-12-12 17:37:43 +0800

    vv

--- vue.config.js
+++ vue.config.js
@@ -58,7 +58,7 @@ module.exports = {
   },
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
-    // it can be accessed in index.html to inject the correct title.
+    // it can be accessed in index1.html to inject the correct title.
     name: name,
     resolve: {
       alias: {
