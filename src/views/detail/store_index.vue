1afbd858e0971ac7297d98aafa75a2cc822fa058
hwhany123 <253922804@qq.com>
2023-02-11 22:53:32 +0800

    2023021101

--- src/views/detail/store_index.vue
+++ src/views/detail/store_index.vue
@@ -11,7 +11,7 @@
         <el-col :span="4" >折扣： <div class="grid-content bg-purple">{{ article.discount }}</div> </el-col>
       </el-row>
       <el-row type="flex" class="row-bg" justify="center"><img src="https://s2.cn.bing.net/th?id=OJ.AR0cTRmf9y09BQ&w=120&h=120&c=8&rs=1&pid=academic" /></el-row>浏览图片
-      <el-row type="flex" class="row-bg" justify="center"><img :src="article.avatar.content" /></el-row>
+      <el-row type="flex" class="row-bg" justify="center"><img :src="article.avatar.content" with="50%" /></el-row>
       <div v-html="article.body_html" class="article-body"></div>
 
     </div>
