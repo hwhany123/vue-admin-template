bae0cbff01a266511801fd3fc1bd2289dd505a23
hwhany123 <253922804@qq.com>
2023-06-12 22:34:03 +0800

    v提交2023061201

---
+++ src/views/detail/index_bxclf.vue
@@ -0,0 +1,218 @@
+<template>
+  <div class="grid-container">
+    <div>
+      </br>
+      <div id="pzh"><div>报销单号：{{ list.id }}</div><div></br></div><div>出差事由：{{ list.事由 }}</div></div>
+      </br>
+
+
+      <div id="pzh" class="row">
+        <div class="col-lg-3">报销人：{{ list.报销人 }}</div>
+        <div class="col-lg-3">部门：{{ list.部门 }}</div>
+        <div class="col-lg-3">是否享受车改：<input type="checkbox" :checked="list.是否享受车改" disabled=True></div>
+        <div class="col-lg-3">是否公派车辆/；<input type="checkbox" :checked="list.是否公派车辆" disabled=True></div>
+      </div>
+      </br>
+
+      </br>
+      <div>
+
+        </br>
+        <el-table v-loading="listLoading" :data="list.行项目" element-loading-text="Loading" border fit highlight-current-row :header-cell-style="{background:'#778899',color:'white'}" show-summary=True>
+
+          <el-table-column align="center" label="序号" width="95">
+            <template slot-scope="scope">
+              {{ scope.row.id}}
+            </template>
+          </el-table-column>
+          <el-table-column label="起始日期" align="center">
+            <template slot-scope="scope">
+              <div v-if="scope.row.起始日期">
+                {{ scope.row.起始日期}}
+              </div>
+            </template>
+          </el-table-column>
+          <el-table-column label="结束日期" align="center">
+            <template slot-scope="scope">
+              <div v-if="scope.row.结束日期">
+                {{ scope.row.结束日期}}
+              </div>
+            </template>
+          </el-table-column>
+          <el-table-column label="出发地" align="center" width="60px">
+            <template slot-scope="scope">
+              <span>{{ scope.row.出发地}}</span>
+            </template>
+          </el-table-column>
+          <el-table-column label="目的地" align="center">
+            <template slot-scope="scope">
+              <div v-if="scope.row.目的地 !== null">
+                {{ scope.row.目的地}}
+              </div>
+            </template>
+          </el-table-column>
+
+          <el-table-column label="补贴" align="center">
+            <template slot-scope="scope">
+
+
+                {{ scope.row.补贴}}
+
+            </template>
+          </el-table-column>
+          <el-table-column label="交通工具类型" align="center">
+            <template slot-scope="scope">
+              <!--div @click="htm(x=scope.row.id)">{{ scope.row.title}}</div-->
+
+              {{ scope.row.交通工具类型}}
+
+            </template>
+          </el-table-column>
+          <el-table-column label="交通费" align="center">
+            <template slot-scope="scope">
+              <!--div @click="htm(x=scope.row.id)">{{ scope.row.title}}</div-->
+
+              {{ scope.row.交通费}}
+
+            </template>
+          </el-table-column>
+          <el-table-column label="住宿地点" align="center">
+            <template slot-scope="scope">
+              <!--div @click="htm(x=scope.row.id)">{{ scope.row.title}}</div-->
+
+              {{ scope.row.住宿地点}}
+
+            </template>
+          </el-table-column>
+          <el-table-column label="住宿费" align="center">
+            <template slot-scope="scope">
+              <!--div @click="htm(x=scope.row.id)">{{ scope.row.title}}</div-->
+
+              {{ scope.row.住宿费}}
+
+            </template>
+          </el-table-column>
+          <el-table-column label="其他费用" align="center">
+            <template slot-scope="scope">
+              <!--div @click="htm(x=scope.row.id)">{{ scope.row.title}}</div-->
+
+              {{ scope.row.其他费用}}
+
+            </template>
+          </el-table-column>
+          <el-table-column label="文章内容" align="center" width="60px">
+            <template slot-scope="scope">
+              <div>
+                <router-link :to="{ name: 'ArticleDetail_bxclf', params: { id: scope.row.id }}">
+                  -详情-
+                </router-link>
+              </div>
+            </template>
+          </el-table-column>
+          <el-table-column label="Actions" align="center" width="330" class-name="small-padding fixed-width">
+            <template slot-scope="{row,$index}">
+              <!--el-button type="primary" size="mini" @click="handleUpdate(row)">
+                Edit
+              </el-button-->
+              <el-button type="primary" size="mini">
+                <router-link :to="{ name: 'ArticleEdit', params: { id: row.id }}">
+                  编辑
+                </router-link>
+              </el-button>
+              <el-button v-if="row.status!='deleted'" size="mini" type="danger" @click="handleDelete(row,$index)">
+                删除
+              </el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+  import {
+    getList,
+    getDetail
+  } from '@/api/table_clbx'
+  import Comments from '@/components/comment/Comments.vue'
+
+  export default {
+    components: {Comments,},
+    data() {
+      return {
+        //listLoading: true,
+        list: null,
+      }
+    },
+    mounted() {
+      this.fetchData()
+    },
+    methods:{
+      fetchData() {
+        getDetail(this.$route.params.id).then(response => {
+
+        this.list = response
+        console.log("差旅报销",this.list)
+        })
+      },
+      hh(args){
+        console.log(args)
+        return {
+          backgroundColor:'red'
+        }
+
+      },
+      formatted_time: function (iso_date_string) {
+        const date = new Date(iso_date_string);
+        return date.toLocaleDateString()},
+    },
+  }
+</script>
+
+<style scoped>
+    .grid-container {
+        display: grid;
+        grid-template-columns: 3fr 1fr;
+        margin-left: 20px;
+    }
+
+
+    #title {
+        text-align: center;
+        font-size: x-large;
+    }
+    #pzh {
+        text-align: center;
+        font-size: small;
+        color: gray;
+    }
+
+    #subtitle {
+        text-align: center;
+        color: gray;
+        font-size: small;
+    }
+
+</style>
+
+<style>
+    .article-body p img {
+        max-width: 100%;
+        border-radius: 50px;
+        box-shadow: gray 0 0 20px;
+    }
+
+    .toc ul {
+        list-style-type: none;
+    }
+
+    .toc a {
+        color: gray;
+    }
+    .row {
+      display: flex;
+      justify-content: space-around;
+    }
+</style>
---
+++ src/views/detail/index_xsz.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="grid-container">
+    <div>
+      <h1 id="title">{{ list.摘要 }}</h1>
+      <div id="pzh">{{ list.年 }}年{{ list.月 }}月----{{ list.凭证号 }}</div>
+      <div>
+        <table border="1">
+          <tr>
+            <th>科目名称</th>
+            <th v-if="list.费用">费用</th>
+            <th v-if="list.新部门">部门</th>
+            <th v-if="list.客商">客商</th>
+            <th v-if="list.人员">人员</th>
+            <th v-if="list.项目">项目</th>
+            <th v-if="list.安全环保投入范围">安全环保投入范围</th>
+            <th v-if="list.成本对象">成本对象</th>
+            <th v-if="list.物料名称">物料名称</th>
+            <th v-if="list.税率">税率</th>
+            <th v-if="list.银行账户">银行账户</th>
+            <th>摘要</th>
+            <th>借方</th>
+            <th>贷方</th>
+          </tr>
+          <tr>
+            <td>{{ list.科目名称 }}</td>
+            <td v-if="list.费用">{{ list.费用 }}</td>
+            <td v-if="list.新部门">{{ list.新部门 }}</td>
+            <td v-if="list.客商">{{ list.客商 }}</td>
+            <td v-if="list.人员">{{ list.人员 }}</td>
+            <td v-if="list.项目">{{ list.项目 }}</td>
+            <td v-if="list.安全环保投入范围">{{ list.安全环保投入范围 }}</td>
+            <td v-if="list.成本对象">{{ list.成本对象 }}</td>
+            <td v-if="list.物料名称">{{ list.物料名称 }}</td>
+            <td v-if="list.税率">{{ list.税率 }}</td>
+            <td v-if="list.银行账户">{{ list.银行账户 }}</td>
+            <td>{{ list.摘要 }}</td>
+            <td>{{ list.借方 }}</td>
+            <td>{{ list.贷方 }}</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+  import {
+    getList,
+    getDetail
+  } from '@/api/table_xsz'
+  import Comments from '@/components/comment/Comments.vue'
+
+  export default {
+    components: {Comments,},
+    data() {
+      return {
+        //listLoading: true,
+        list: null,
+      }
+    },
+    mounted() {
+      this.fetchData()
+    },
+    methods:{
+      fetchData() {
+        getDetail(this.$route.params.id).then(response => {
+
+        this.list = response
+        console.log("不加干预i给",this.list)
+        })
+      },
+      formatted_time: function (iso_date_string) {
+        const date = new Date(iso_date_string);
+        return date.toLocaleDateString()},
+    },
+  }
+</script>
+
+<style scoped>
+    .grid-container {
+        display: grid;
+        grid-template-columns: 3fr 1fr;
+        margin-left: 20px;
+    }
+
+
+    #title {
+        text-align: center;
+        font-size: x-large;
+    }
+    #pzh {
+        text-align: center;
+        font-size: small;
+        color: gray;
+    }
+
+    #subtitle {
+        text-align: center;
+        color: gray;
+        font-size: small;
+    }
+
+</style>
+
+<style>
+    .article-body p img {
+        max-width: 100%;
+        border-radius: 50px;
+        box-shadow: gray 0 0 20px;
+    }
+
+    .toc ul {
+        list-style-type: none;
+    }
+
+    .toc a {
+        color: gray;
+    }
+</style>
