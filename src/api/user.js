a3a072fd014682518771d495c839386ec48a59ac
21704 <253922804@qq.com>
2024-03-15 01:22:32 +0800

    2024031501

--- src/api/user.js
+++ src/api/user.js
@@ -10,7 +10,7 @@ export function login(data) {
 
 export function getInfo(token) {
   return request({
-    url: '/api/user/admin/',
+    url: '/api/user/huangwenhua/',
     method: 'get',
     params: { token }
   })
